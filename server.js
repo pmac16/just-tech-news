a0e919e557d467f4e4a969d9173b971c9ed59ace
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-03-03 09:21:04 -0800

    created api routes

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//turn on routes
+// turn on routes
 app.use(routes);
 
-//turn on connection to db and server
-sequelize.sync({force:false}).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
