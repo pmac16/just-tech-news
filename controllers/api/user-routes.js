878cd7d8b77f777eb6db73f4795e785cdd88eb7b
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-03-12 11:57:21 -0800

    sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,13 +60,16 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => {
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
       res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
     });
+  })
+    
 });
 
 router.post('/login', (req, res) => {
@@ -76,20 +79,27 @@ router.post('/login', (req, res) => {
       email: req.body.email
     }
   }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
-      return;
-    }
+  if (!dbUserData) {
+    res.status(400).json({ message: 'No user with that email address!' });
+    return;
+  }
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
+  const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+  if (!validPassword) {
+    res.status(400).json({ message: 'Incorrect password!' });
+    return;
+  }
+
+  req.session.save(() => {
+    // declare session variables
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
 
     res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
+});
 });
 
 router.put('/:id', (req, res) => {
